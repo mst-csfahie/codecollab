7c2deb125306ea0ae885c3e5684eb14f38e62356
CodeImitatingLife <35890917+CodeImitatingLife@users.noreply.github.com>
2023-08-03 16:49:11 -0400

    another alteration to Hello.java
    version-1.2.0: added a new method.

--- Hello.java
+++ Hello.java
@@ -3,5 +3,11 @@ public class Hello{
     public static void main(String[] args){
       System.out.println("Just saying \"Hello\" !");
       System.out.println("Hello to you, too!");
+      // trying something new...
+      System.out.println(newMsg);
+    }
+
+    public static String newMsg(){
+        String str="Could this possibly work?");
     }
 }
